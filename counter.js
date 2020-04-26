56ef8e5d4043e7a194cdddcb7096fe922bca49b2
kafkasdog4 <46569017+kafkasdog4@users.noreply.github.com>
2020-04-26 09:12:40 -0500

    multi

--- counter.js
+++ counter.js
@@ -2,10 +2,9 @@
 function clock() {
     setInterval(function() {
         let d = new Date();
+        let dateString = d.toString().split(d.getFullYear())[0].concat(d.getFullYear());
         const time = d.getHours() < 12 ? "AM" : "PM";
-        let dateTimeString = d.getFullYear() + " " + 
-            d.getMonth().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + " " + 
-            d.getDate().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + "<p></p>" + (
+        let dateTimeString = dateString + "<p></p>" + (
             d.getHours() > 12 ? d.getHours() % 12 : d.getHours()).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})+ ":" + 
             d.getMinutes().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + ":" + 
             d.getSeconds().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + " " + time;
